812cf70beea70e357877114f64acc022bcfb915e
xxc <x007007007@hotmail.com>
2017-11-19 01:07:28 +0800

    pip wheel package fix

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(
     license='MIT',
     url='https://github.com/x007007007/pyrequirements/',
     classifiers=[
-        'Environment :: Raspberry',
         'Intended Audience :: Developers',
         'Operating System :: Linux',
         'Operating System :: POSIX',
