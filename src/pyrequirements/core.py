12837894a54db402ade7994bc9a010e7549c528d
xxc <xingci.xu@autodesk.com>
2017-11-19 18:19:11 +0800

    py3 bug fix

--- src/pyrequirements/core.py
+++ src/pyrequirements/core.py
@@ -13,24 +13,30 @@ def freeze_version(*args, **kwargs):
     data_map = OrderedDict()
     package_index = {}
     result = []
-    package_names = args
-    package_name_vers = kwargs
+    package_names = [v.lower() for v in args]
+    package_name_vers = {k.lower(): v for k, v in kwargs.items()}
     for installed in pip.get_installed_distributions():
-        package_index[installed.project_name] = installed
+        package_index[installed.project_name.lower().strip()] = installed
 
     for pkg_name in itertools.chain(package_names, package_name_vers.keys()):
+        pkg_name = pkg_name.lower().strip()
         if pkg_name not in package_index:
             warnings.warn("current python environment don't install required package: {}".format(pkg_name), UserWarning)
             data_map[pkg_name] = None
+            continue
         if pkg_name in package_names:
             if pkg_name not in data_map:
-                data_map[pkg_name] = {'pkg': package_index[pkg_name]}
+                data_map[pkg_name] = {
+                    'pkg': package_index[pkg_name],
+                    'project_name': package_index[pkg_name].project_name
+                }
             else:
                 raise ReferenceError("duplicate requirement: {}".format(pkg_name))
         if pkg_name in package_name_vers:
             if pkg_name not in data_map:
                 data_map[pkg_name] = {
                     'pkg': package_index[pkg_name],
+                    'project_name': package_index[pkg_name].project_name,
                     'version': package_name_vers[pkg_name],  # kwargs version
                 }
             else:
@@ -38,10 +44,11 @@ def freeze_version(*args, **kwargs):
 
     for pkg_name, info in data_map.items():
         if info is None:
+            print(pkg_name)
             result.append(pkg_name)
         elif "version" in info:
-            result.append("{}{}".format(pkg_name, info["version"]))
+            result.append("{}{}".format(info['project_name'], info["version"]))
         else:
-            result.append("{}=={}".format(pkg_name, info["pkg"].version))
+            result.append("{}=={}".format(info['project_name'], info["pkg"].version))
     return result
 
--- src/pyrequirements/main.py
+++ src/pyrequirements/main.py
@@ -8,7 +8,8 @@ def install():
     config_path = os.path.join(os.curdir, "requirements.ini")
     if not os.path.exists(config_path):
         with open(config_path, "w") as fp:
-            fp.write(pkg_resources.resource_string("pyrequirements", "resource/requirements.ini"))
+            fp.write(pkg_resources.resource_string("pyrequirements", "resource/requirements.ini").decode('utf-8'))
+
 
 def upgrade(name=None):
     print("You should manual run `pip install --upgrade -r requirements.txt`")
